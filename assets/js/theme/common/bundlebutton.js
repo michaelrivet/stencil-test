88d41809762a675cf97fac35e1e495de700a01d7
mikedichello <michael.dichello@gmail.com>
2020-05-31 23:57:43 -0500

    working on api requests

--- assets/js/theme/common/bundlebutton.js
+++ assets/js/theme/common/bundlebutton.js
@@ -1,30 +1,62 @@
+const cartURL = "/api/storefront/carts";
 
-
-// var bundleBasket = ["SLLPJ-20D88EFC", "CC3C", "OTL"];
-
-
-var bundleBasket = [77, 93, 104];
-
-const cartUrl = 'https://stencil-test.mybigcommerce.com/cart.php';
-
-const addToCart = function() {
-    if (bundleBasket.length) {
-      for (var i=0; i < bundleBasket.length; i++) {
-        $.ajax({
-          type: 'GET',
-          async: false,
-          url: cartUrl+'?action=add&amp;product_id='+bundleBasket[i]+'&amp;fastcart=1&amp;ajaxsubmit=1',
-          success: function(data) {
-            var obj = JSON.parse($(data).html());
-            if (obj.success) {
-            } else {
-              console.log("error")
-            }
-          }
-        });
-      }
+const bundleItems = {
+    "lineItems": [
+    {
+        "quantity": 1,
+        "productId": 88
+    },
+    {
+        "quantity": 1,
+        "productId": 104
+    },
+    {
+        "quantity": 1,
+        "productId": 93,
+        "variantId": 54
     }
-    window.location.replace(cartUrl);
-  }; 
+]};
+
+function getCart(url) {
+    return fetch(url, {
+        method: "GET",
+        credentials: "same-origin",
+        headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+    })
+    .then(response => response.json());
+ };
+
+//  let cart = getCart('/api/storefront/carts?include=lineItems.digitalItems.options,lineItems.physicalItems.options');
+                
+//  let cartId = cart[0].id;
+
+ function addCartItem(url, cartId, cartItems) {
+    return fetch(url + '/' + cartId + '/items', {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+            'Accept': 'application/json'},
+        body: JSON.stringify(cartItems),
+    })
+    .then(response => response.json());
+};
+
+const bundleAdd = () => {
+    let cart = getCart('/api/storefront/carts?include=lineItems.digitalItems.options,lineItems.physicalItems.options')
+        .then(data => console.log(JSON.stringify(data)))
+                
+    let cartId = cart[0].id;
+
+    addCartItem(cartURL, cartId, bundleItems)
+      .then(data => console.log(JSON.stringify(data)))
+      .catch(error => console.error(error));
+
+};
+
+
   
-$('#bundle-button').click(addToCart);
+$('#bundle-button').click(() => bundleAdd());
